869de34423141f17ef8e5991b32068842bd2599d
Kim-Minhwan423 <day9brewinco@gmail.com>
2025-01-31 21:37:29 +0900

    Update 2-chengla-yogiyo-auto.py

--- 2-chengla-yogiyo-auto.py
+++ 2-chengla-yogiyo-auto.py
@@ -168,7 +168,7 @@ def close_popup_if_exist(driver):
     로그인 후 뜨는 팝업(#portal-root 등등) 닫기
     예시 CSS: #portal-root > div > ...
     """
-    popup_close_selector = "#modal > div > div > div.sc-f54b6194-1.fCrjsm > svg"
+    popup_close_selector = "#modal > div > div > div.sc-f54b6194-1.fCrjsm > svg > g"
     try:
         close_btn = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, popup_close_selector))
