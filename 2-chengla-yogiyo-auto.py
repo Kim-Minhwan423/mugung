e196d26fdaeb04f1e1784cbd98c40704cf6c65ba
Kim-Minhwan423 <day9brewinco@gmail.com>
2025-02-01 22:20:39 +0900

    Update 2-chengla-yogiyo-auto.py

--- 2-chengla-yogiyo-auto.py
+++ 2-chengla-yogiyo-auto.py
@@ -324,7 +324,6 @@ def main():
         go_order_history(driver)
         
         # 4. 오늘 주문 처리 (주문금액 및 품목 정보 집계)
-        logging.info(f"오늘 주문 총액: {total_order}, 제품별 수량: {product_quantities}")
         
         # 5. Google Sheets 업데이트 (일일 정산 및 재고)
         update_google_sheets(total_order, product_quantities)
