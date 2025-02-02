58584813f34dcb9ea46bd06d81e70a3dfc878760
Kim-Minhwan423 <day9brewinco@gmail.com>
2025-02-02 13:34:19 +0900

    Update 2-chengla-yogiyo-auto.py

--- 2-chengla-yogiyo-auto.py
+++ 2-chengla-yogiyo-auto.py
@@ -99,7 +99,7 @@ def get_chrome_driver(use_profile=False):
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--window-size=1029,657")
+    chrome_options.add_argument("--window-size=1529,957")
     
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
