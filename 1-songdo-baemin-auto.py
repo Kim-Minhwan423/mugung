1b7e8413980d902901488ebd3f69ccd7dd23e5e4
Kim-Minhwan423 <day9brewinco@gmail.com>
2025-02-04 22:53:50 +0900

    Update 1-songdo-baemin-auto.py

--- 1-songdo-baemin-auto.py
+++ 1-songdo-baemin-auto.py
@@ -269,10 +269,10 @@ def login_and_close_popup(driver, wait, username, password):
     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, menu_button_selector)))
     logging.info("로그인 성공")
 
-    popup_close_selector = r"#\:rp\: > div.Container_c_9rpk_1utdzds5.OverlayFooter_b_9yfm_1slqmfa0 > div > div > button"
+    popup_close_xpath = f"//*[@id=':rl:']/div[3]/div/div/button"
     try:
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, popup_close_selector)))
-        driver.find_element(By.CSS_SELECTOR, popup_close_selector).click()
+        wait.until(EC.element_to_be_clickable((By.XPATH, popup_close_xpath)))
+        driver.find_element(By.XPATH, popup_close_xpath).click()
         logging.info("팝업 닫기 성공")
     except TimeoutException:
         logging.info("닫을 팝업이 없음 (스킵)")
