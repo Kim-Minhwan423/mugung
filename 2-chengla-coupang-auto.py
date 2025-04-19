7990db254ce231218cbaab4917ab62aa4b735fda
Kim-Minhwan423 <day9brewinco@gmail.com>
2025-04-19 14:18:11 +0900

    Update 2-chengla-coupang-auto.py

--- 2-chengla-coupang-auto.py
+++ 2-chengla-coupang-auto.py
@@ -92,7 +92,7 @@ def get_chrome_driver(use_profile=False):
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--window-size=1200,960")
+    chrome_options.add_argument("--window-size=1300,960")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
