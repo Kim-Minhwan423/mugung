21f6c0815dae3b0a007a52859d28e7f4aee5d294
Kim-Minhwan423 <day9brewinco@gmail.com>
2025-04-19 14:20:48 +0900

    Update 1-songdo-coupang-auto.py

--- 1-songdo-coupang-auto.py
+++ 1-songdo-coupang-auto.py
@@ -75,7 +75,7 @@ def get_environment_variables():
 ###############################################################################
 def get_chrome_driver(use_profile=False):
     chrome_options = webdriver.ChromeOptions()
-   # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
 
     # User-Agent 변경
     chrome_options.add_argument(
@@ -98,7 +98,7 @@ def get_chrome_driver(use_profile=False):
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--window-size=1280,960")
+    chrome_options.add_argument("--window-size=1300,960")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
