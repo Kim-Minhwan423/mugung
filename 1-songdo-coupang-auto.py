b0bc27846e7d1ebb6bcde31fe000ffe5846ddc36
Kim-Minhwan423 <day9brewinco@gmail.com>
2025-04-19 14:34:25 +0900

    Update 1-songdo-coupang-auto.py

--- 1-songdo-coupang-auto.py
+++ 1-songdo-coupang-auto.py
@@ -98,7 +98,7 @@ def get_chrome_driver(use_profile=False):
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--window-size=1270,960")
+    chrome_options.add_argument("--window-size=1280,960")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
