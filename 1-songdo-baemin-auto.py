15ce57316c64e595f3c6121505cf7373c1e761b7
Kim-Minhwan423 <day9brewinco@gmail.com>
2025-05-15 22:08:09 +0900

    Update 1-songdo-baemin-auto.py

--- 1-songdo-baemin-auto.py
+++ 1-songdo-baemin-auto.py
@@ -264,7 +264,7 @@ def login_and_close_popup(driver, wait, username, password):
     login_button_selector = "#root > div.style__LoginWrap-sc-145yrm0-0.hKiYRl > div > div > form > button"
     driver.find_element(By.CSS_SELECTOR, login_button_selector).click()
     logging.info("로그인 버튼 클릭")
-    time.sleep(20)
+    time.sleep(5)
 
     popup_close_selector = ("div[id^='\\:r'] div.Container_c_b149_1utdzds5.OverlayFooter_b_b8ew_1slqmfa0.OverlayFooter_b_b8ew_1slqmfa1 > div.Flex_c_b149_bbdidai.Flex_c_b149_bbdidak.Flex_c_b149_bbdida2 > button.TextButton_b_b8ew_1j0jumh3.c_b149_13ysz3p2.c_b149_13ysz3p0.TextButton_b_b8ew_1j0jumh6.TextButton_b_b8ew_1j0jumhb.c_b149_13c33de3")
     try:
