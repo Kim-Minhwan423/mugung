7b13acb0ce635d97307e5ffd8934fd2a1ce38770
Kim-Minhwan423 <day9brewinco@gmail.com>
2025-10-10 17:32:31 +0900

    Update 2-chengla-yogiyo-auto.py

--- 2-chengla-yogiyo-auto.py
+++ 2-chengla-yogiyo-auto.py
@@ -428,6 +428,8 @@ def update_google_sheets(total_order_amount, aggregated_products):
     sheet_inventory.batch_clear(clear_ranges)
 
     update_mapping = {
+        '낙지덮밥': 'AF40',
+        '낙지볶음': 'AF39',
         '육회비빔밥': 'Q43',
         '꼬리곰탕': 'F38',
         '빨간곰탕': 'F39',
