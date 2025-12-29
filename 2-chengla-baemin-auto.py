9933df5de71de21322c9d77978dac75658f22871
Kim-Minhwan423 <day9brewinco@gmail.com>
2025-12-29 23:32:27 +0900

    Update 2-chengla-baemin-auto.py

--- 2-chengla-baemin-auto.py
+++ 2-chengla-baemin-auto.py
@@ -315,7 +315,7 @@ def login_and_close_popup(driver, wait, username, password):
     driver.find_element(By.CSS_SELECTOR, login_button_selector).click()
     logging.info("로그인 버튼 클릭")
 
-    time.sleep(30000)
+    time.sleep(3)
 
     popup_close_selector = ("div[id^='\\:r'] div.Container_c_c1xs_1utdzds5.OverlayFooter_b_c9kn_1slqmfa0 > div > button.TextButton_b_c9kn_1j0jumh3.c_c1xs_13ysz3p2.c_c1xs_13ysz3p0.TextButton_b_c9kn_1j0jumh6.TextButton_b_c9kn_1j0jumhb.c_c1xs_13c33de3")
     try:
@@ -334,13 +334,13 @@ def login_and_close_popup(driver, wait, username, password):
         logging.info("팝업이 없거나 이미 닫힘")
 
 def navigate_to_order_history(driver, wait):
-    menu_button_selector = "#root > div > div.Container_c_qx9u_1utdzds5.MobileHeader-module__Zr4m > div > div > div:nth-child(1) > button"
+    menu_button_selector = "#root > div > div.Container_c_qx9u_1utdzds5.MobileHeader-module__Zr4m > div > div > div:nth-child(1) > button > span > span > svg"
     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, menu_button_selector)))
     driver.find_element(By.CSS_SELECTOR, menu_button_selector).click()
 
     time.sleep(3)
     
-    order_history_selector = "#root > div > div.frame-container.lnb-open > div.frame-aside > nav > div.LNBList-module__DDx5.LNB-module__whjk > div.Container_c_c1xs_1utdzds5 > a:nth-child(18) > button"
+    order_history_selector = "#root > div > div.frame-container.lnb-open > div.frame-aside > div > nav > div.LNBList-module__DDx5.LNB-module__whjk > div.Container_c_qx9u_1utdzds5 > a:nth-child(18) > button"
     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, order_history_selector)))
     driver.find_element(By.CSS_SELECTOR, order_history_selector).click()
     
