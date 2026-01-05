8a11568ff0924f6d7a4da1638652292d67d4b45f
Kim-Minhwan423 <day9brewinco@gmail.com>
2026-01-05 20:19:05 +0900

    Update 1-songdo-ok-pos-auto.py

--- 1-songdo-ok-pos-auto.py
+++ 1-songdo-ok-pos-auto.py
@@ -168,7 +168,7 @@ def main():
         # 5. 즐겨찾기 → 일자별 → 상품별 일매출분석
         # ================================================
         # 즐겨찾기 탭
-        WebDriverWait(erWair, 10).un,il(
+        WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, "#divTopFrameHead > div:nth-child(2) > div:nth-child(2)"))
         )
         favorite_tab = driver.find_element(
