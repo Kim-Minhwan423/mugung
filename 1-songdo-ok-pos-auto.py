12f458819733027746a2acd9db74c1e23e8d1313
Kim-Minhwan423 <day9brewinco@gmail.com>
2026-01-07 14:40:17 +0900

    Update 1-songdo-ok-pos-auto.py

--- 1-songdo-ok-pos-auto.py
+++ 1-songdo-ok-pos-auto.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 TIMEOUT = 10
 
 # =====================================================
@@ -285,7 +286,7 @@ def main():
         options.binary_location = "/usr/bin/google-chrome"
 
         driver = webdriver.Chrome(
-            service=ChromeService(),
+            service=ChromeService(ChromeDriverManager().install()),
             options=options
         )
 
