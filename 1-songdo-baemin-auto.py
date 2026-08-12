2f35097f168d06d0cbbb4363f84e3d1a37dd8449
Kim-Minhwan423 <day9brewinco@gmail.com>
2026-08-12 18:03:17 +0900

    Update 1-songdo-baemin-auto.py

--- 1-songdo-baemin-auto.py
+++ 1-songdo-baemin-auto.py
@@ -518,9 +518,11 @@ def login_and_close_popup(driver, wait, username, password):
     # 7. 로그인 후 팝업 닫기
     # ------------------------------------------------------------------
     popup_close_selector = (
-    "div[id^='\\:r'] > div.Container_c_qx9u_1utdzds5.OverlayHeader_b_r4ax_5xyph30.c_qx9u_13c33de0 "
-    "> div.OverlayHeader_b_r4ax_5xyph31.c_qx9u_13c33de0.c_qx9u_13ysz3p2.c_qx9u_13ysz3p0 "
-    "> div:nth-child(1) > button > span > svg"
+    "div.OverlayHeader_b_r4ax_5xyph31."
+    "c_qx9u_13c33de0."
+    "c_qx9u_13ysz3p2."
+    "c_qx9u_13ysz3p0 "
+    "div:nth-child(1) > button"
 )
 
     for popup_selector in popup_close_selectors:
