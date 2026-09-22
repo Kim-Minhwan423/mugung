d5b727fd634e787d8366f908e7a435b6fbc65183
Kim-Minhwan423 <day9brewinco@gmail.com>
2026-09-22 18:23:44 +0900

    Update 2-chengla-yogiyo-auto.py

--- 2-chengla-yogiyo-auto.py
+++ 2-chengla-yogiyo-auto.py
@@ -329,7 +329,7 @@ def get_todays_orders(driver):
 
             for n in range(1, 11):  # 최대 10개까지 시도
                 try:
-                    item_selector = 
+                    item_selector = (
                         f"{base_selector} > div:nth-child(2) > div > div  > "
                         "div.OrderDetailPopup__OrderFeeItemContent-sc-cm3uu3-11.ghPAZZ > "
                         "div:nth-child({n}) > div.OrderDetailPopup__OrderFeeItemContent-sc-cm3uu3-15.fnJncm > span:nth-child(1)"
